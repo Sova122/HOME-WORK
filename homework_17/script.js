05352576801c46303bafc069ecccedd323b16893
Sova122 <sovka1227@gmail.com>
2019-07-12 22:56:48 +0300

    done

---
+++ homework_17/script.js
@@ -0,0 +1,34 @@
+// первое задание
+let s = 60;
+let m = 60;
+let h = s*m;
+console.log(3*h);
+
+// второе задание
+let num = 1;
+num+=12;
+num-=14;
+num*=5;
+num/=7;
+num++;
+num--;
+alert(num);
+
+//третье задание
+let a = 10;
+let b = 2;
+let c = 5;
+console.log(a + b + c);
+
+//четвертое задание
+a = 17; 
+b = 10;
+c = a - b;
+d = 7;
+result = c + d;
+console.log(result);
+
+//пятое задание
+let name = "Андрей";
+let hello = "Привет,"
+console.log(hello + ' ' + name + '!');
