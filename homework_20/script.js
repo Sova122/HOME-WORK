ee7f00dfddda53dde2cc520f3082dc0c385e1e6a
Sova122 <sovka1227@gmail.com>
2019-07-26 23:27:10 +0300

    js20

---
+++ homework_20/script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+// Первое задание
+
+function fib(n) {
+    if( n < 2 ){
+        return n;
+    }   return ( fib(n-1) + fib(n-2) );
+  
+  } 
+
+console.log( fib(3) ); 
+console.log( fib(4) );
+console.log( fib(5) ); 
+
+// Второе задание
+
+
+let i = 1000;
+
+let z = 0;
+
+while (i >= 50) {
+    i /= 2;
+    z ++;
+}
+
+console.log( i );
+console.log( z );
+
+
+
+
