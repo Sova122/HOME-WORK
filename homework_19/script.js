efe4ce1f2185b5031ec3ca4c0ed0351f672fb4a9
Sova122 <sovka1227@gmail.com>
2019-07-26 19:22:30 +0300

    js19

--- homework_19/script.js
+++ homework_19/script.js
@@ -1,40 +1,44 @@
 "use strict";
 // Первое задание
 
-function isNumberInRange(a  = 22)  {
-    if (a > 0 && a < 10) console.log(true);
-    else console.log(false);  
+function isNumberInRange( a  = 22 )  {
+    if ( a > 0 && a < 10 ) {
+        return true;
+    }   return false;
+  
 } 
 
 isNumberInRange();
 
 // Второе задание
 
-function isEven(с=1) {
+function isEven( с=1 ) {
     if( с%2 === 0 ){
         return true;
-    }else
+    }
     return false;
 }
 
-console.log(isEven());
+console.log( isEven() );
+
 
 // Третье задание
 
 
-let value = +prompt('value?', '');
-switch(value){
+let value = +prompt( 'value?', '' );
+
+switch ( value ){
     case 0:
-        alert('0');
+        alert( '0' );
         break;
 
     case 1:
-        alert('1');
+        alert( '1' );
         break;
 
     case 2:
     case 3:
-        alert('2,3');
+        alert( '2,3' );
         break;
 }
 
@@ -42,9 +46,10 @@ switch(value){
 
 
 
-function min(a, b){
+function min( a, b ) {
     return( a < b ? a : b );
 }
+
 console.log(min(3,5));
 console.log(min(5,-1));
 
